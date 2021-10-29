9c11d79d6191db8abe242c8d433abbb91c405cf3
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-10-29 18:27:38 +0900

    python記述の基礎学習

--- script.py
+++ script.py
@@ -1,2 +1,21 @@
-print('Hello, world!')
-print(1 + 2)
+def add_world(word):
+    print(word + 'World')
+# インデントはスペース4つ
+add_world('Hello')
+
+
+apple = 100
+money = 300
+input_count = input("リンゴを何個買うかを入力してください:")
+# int()データ型変更　int = 整数, str = 文字列
+count = int(input_count)
+total_price = apple * count
+if total_price < money:
+    print("合計" + str(total_price) + "円です")
+    print("残高は" + str(money - total_price) + "円です")
+elif total_price == money:
+    print("合計" + str(total_price) + "円です")
+    print("残高はありません")
+else:
+    print(str(total_price - money) + "円足りませんでした")
+    print("りんごを買えませんでした")
---
+++ script2.py
@@ -0,0 +1,26 @@
+fruits = ['リンゴ', 'バナナ', 'ブドウ']
+
+# for文
+for i in fruits:
+    print(i)
+
+for i in fruits:
+    print(i)
+    if i == 'バナナ':
+        break
+
+
+# while文
+i = 0
+
+while i < 4:
+    print(i)
+    i += 1
+
+i = 0
+
+while i < 4:
+    print(i)
+    if i == 2:
+        break
+    i += 1
